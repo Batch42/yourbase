99ecb7bf710a4985fe3429f834e2453a0297f427
Batch <batch@Simpletron.resnet.smcm.edu>
2018-01-22 20:50:21 -0500

    Added python example of bazel.

---
+++ examples/pypinger/client/pinger.py
@@ -0,0 +1,11 @@
+from socket import *
+
+password = 'ping!ping!ping'
+
+client=socket(AF_INET, SOCK_DGRAM)
+client.sendto(password.encode(),('localhost',12345))
+try:
+        message, server = client.recvfrom(16)
+        print(message.decode())
+except:
+        print ('Request has timed out')
---
+++ examples/pypinger/server/pingserver.py
@@ -0,0 +1,13 @@
+from socket import *
+
+serv=socket(AF_INET, SOCK_DGRAM)
+
+serv.bind(('', 12345))
+
+password = 'ping!ping!ping'
+
+while True:
+
+        message, addr = serv.recvfrom(16)
+        if password == message.decode():
+                serv.sendto('ping'.encode(),addr)
